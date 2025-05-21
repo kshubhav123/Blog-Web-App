673ab9093c1d06ef20f8efd35ab08b2f509a650f
kshubhav123 <kshubhav8@gmail.com>
2025-05-22 00:07:49 +0530

    Initial commit 3

--- server/routes/comment.js
+++
@@ -1,9 +0,0 @@
-const express=require("express");
-const route=express.Router();
-
-const {create,list}=require("../controller/comment");
-
-route.post("/create-comment",create)
-route.get("/comment-list", list)
-
-module.exports=route
